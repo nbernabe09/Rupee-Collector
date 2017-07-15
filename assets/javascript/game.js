e22952b7bfa4b6f31fd098d555349b46877b5509
Nathan Bernabe <nbernabe09@aol.com>
2017-07-15 00:43:06 -0700

    Made functioning game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,11 +10,48 @@ $(document).ready(function() {
 	var rupeeSound = document.createElement("audio");
 	rupeeSound.setAttribute("src", "assets/audio/rupeeHD.wav");
 
-	// Number Items
-	var winCount;
-	var loseCount;
-	var bag;
-	var ranNum;
+	// Number Variables
+	var winCount = 0;
+	var loseCount = 0;
+	var green,
+		blue,
+		red,
+		purple,
+		rupeeBag,
+		ranCost,
+		ranRupee;
+
+	// Game Functions
+	function gameStart() {
+		newRupee();
+		$("#win").text(winCount);
+		$("#lose").text(loseCount);
+		ranCost = Math.floor(Math.random() * (120 - 19) + 19);
+		$("#ran-num").text(ranCost);
+		rupeeBag = 0;
+		$("#bag").text(rupeeBag);
+	}
+	function newRupee() {
+		ranRupee = [];
+		for (var i = 0; i < 4; i++) {
+			var random = Math.floor(Math.random() * 12) + 1;
+			var string = random.toString();
+			ranRupee.push(string);
+		}
+		green = parseInt(ranRupee[0]);
+		blue = parseInt(ranRupee[1]);
+		red = parseInt(ranRupee[2]);
+		purple = parseInt(ranRupee[3]);
+	}
+	function newRound() {
+		if (rupeeBag === ranCost) {
+			winCount ++;
+			gameStart();
+		} else if (rupeeBag > ranCost) {
+			loseCount ++;
+			gameStart();
+		}
+	}
 
 	// Intro
 	$(".game-container").hide();
@@ -24,18 +61,31 @@ $(document).ready(function() {
 	$(".zelda-container").on("click", function() {
 		titleMusic.pause();
 		selectSound.play();
+		gameStart();
 		$(".zelda-container").fadeOut(400, function() {
 			$(".game-container").fadeIn(400, function() {
-				var stopped = false;
 				gameMusic.play();
-				gameMusic.addEventListener('ended', function() {
-					if(!stopped){
-						gameMusic.currentTime = 0;
-						gameMusic.play();
-					}
-				}, false);
+				gameMusic.loop = true;
 			});
 		});
 	});
 
+	// Rupee Mechanics
+	$("#green").on("click", function() {
+		rupeeBag += green;
+	});
+	$("#blue").on("click", function() {
+		rupeeBag += blue;
+	});
+	$("#red").on("click", function() {
+		rupeeBag += red;
+	});
+	$("#purple").on("click", function() {
+		rupeeBag += purple;
+	});
+	$(".rupee").on("click", function() {
+		rupeeSound.play();
+		$("#bag").text(rupeeBag);
+		newRound();
+	});
 });
